aedb9f4c16b3b3a3ec58393f52587ce402c41e3f
Anjara91 <anjaraoramascarrillo@hotmail.com>
2025-01-21 22:12:19 +0100

    Revert "roles creados y primera funcion en trabajadores"

--- BACK/src/api/routers/routes.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-
-router.use("/pedido", require("./api/pedido.router"));
-router.use("/trabajadores", require("../middleware/auth"));
-
-module.exports = router;
