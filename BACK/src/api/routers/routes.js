102adeb177de85448730e0e8bb856feb957a49e6
Anjara91 <anjaraoramascarrillo@hotmail.com>
2025-01-21 18:58:15 +0100

    incluye bbdd

--- BACK/src/api/routers/routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.use("/users", require("./api_routes/user.routes"));
-router.use("/events", require("./api_routes/event.routes"));
-
-module.exports = router;
