3ef96e0cbe92d6a1c811a9ef69d46b917bd089a7
MartaRossado <marta.rossado@gmail.com>
2025-01-23 22:49:14 +0100

    estructuta componente conductor

--- FRONT/src/app/components/home/home.component.ts
+++ FRONT/src/app/components/home/home.component.ts
@@ -3,11 +3,12 @@ import { HomeServiceService } from '../service/home-service.service';
 import { PedidosComponent } from '../pedidos/pedidos.component';
 import { JefeComponent } from '../jefe/jefe.component';
 import { OperarioComponent } from "../operario/operario.component";
+import { ConductorComponent } from "../conductor/conductor.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [PedidosComponent, JefeComponent, OperarioComponent],
+  imports: [PedidosComponent, JefeComponent, OperarioComponent, ConductorComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -17,7 +18,7 @@ export class HomeComponent {
   public pedidosList: any = []
   public userRole = localStorage.getItem('role')
   public name =  localStorage.getItem('nombre')
-  
+
 
 
 
