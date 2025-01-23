4f2e95dea4e5dbe7978cc69bf038f4fe30e739f5
“Paula-fernandez” <a.paula.fdezdiaz@gmail.com>
2025-01-23 19:45:00 +0100

    nuevas funciones

--- BACK/src/api/models/pedido.model.js
+++ BACK/src/api/models/pedido.model.js
@@ -1,7 +1,12 @@
-const insertAlmacenes = async ({localizacion, nombre_nave}) => {
-    console.log("Intentando insertar almacen:", {localizacion, nombre_nave});
+const pool = require("../../utils/db")
+
+
+
+
+const insertarAlmacenes = async ({localizacion}) => {
+    console.log("Intentando insertar almacen:", {localizacion});
     try {
-        const [result] = await pool.query("INSERT INTO almacenes (localizacion, nombre_nave) VALUES (?,?)", [localizacion, nombre_nave]);
+        const [result] = await pool.query("INSERT INTO almacenes (localizacion) VALUES (?)", [localizacion]);
         if (result.affectedRows === 0) {
             return -1;  // Si no se insertó, retorna -1
         }
@@ -10,3 +15,11 @@ const insertAlmacenes = async ({localizacion, nombre_nave}) => {
         
     }
 };
+
+const selectbyLocation = async(localizacion) =>{
+    const result = await pool.query("SELECT * FROM almacenes WHERE localizacion = ?", [localizacion])
+    return result[0]
+}
+
+
+module.exports = {insertarAlmacenes, selectbyLocation}
