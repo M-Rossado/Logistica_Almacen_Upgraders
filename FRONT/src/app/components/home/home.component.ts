302f2bbee8b18f7e1de968e52e9cf2ff3c6c64e2
MartaRossado <marta.rossado@gmail.com>
2025-01-24 09:54:24 +0100

    desesperación con listado conducor

--- FRONT/src/app/components/home/home.component.ts
+++ FRONT/src/app/components/home/home.component.ts
@@ -1,26 +1,22 @@
 import { Component, inject } from '@angular/core';
 import { HomeServiceService } from '../service/home-service.service';
 import { PedidosComponent } from '../pedidos/pedidos.component';
+import { ConductorComponent } from '../conductor/conductor.component';
+import { OperarioComponent } from '../operario/operario.component';
 import { JefeComponent } from '../jefe/jefe.component';
-import { OperarioComponent } from "../operario/operario.component";
-import { ConductorComponent } from "../conductor/conductor.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [PedidosComponent, JefeComponent, OperarioComponent, ConductorComponent],
+  imports: [PedidosComponent, ConductorComponent, OperarioComponent, JefeComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css'] // Corregido: "styleUrls" (en plural)
 })
+
 export class HomeComponent {
-
-  private homeservice: HomeServiceService = inject(HomeServiceService)
-  public pedidosList: any = []
-  public userRole = localStorage.getItem('role')
-  public name =  localStorage.getItem('nombre')
-
-
-
-
-
+  private homeservice: HomeServiceService = inject(HomeServiceService);
+  public pedidosList: any = [];
+  public userRole = localStorage.getItem('role');
+  public name = localStorage.getItem('nombre');
 }
+
