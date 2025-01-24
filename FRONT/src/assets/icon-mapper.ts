588700027a52c027fc451531e85dde7496b02625
juantoniogomezz <j.antonio1893@gmail.com>
2025-01-24 22:07:22 +0100

    versión 1

---
+++ FRONT/src/assets/icon-mapper.ts
@@ -0,0 +1,6 @@
+// Este archivo mapea los status a las clases de Bootstrap Icons
+export const ICON_MAPPER: { [key: string]: string } = {
+    Pendiente: 'bi-check-circle-fill text-success',
+    'En tránsito': 'bi-x-circle-fill text-danger',
+    Completado: 'bi-exclamation-triangle-fill text-warning',
+  };
