a4f10b73c86102b7cc08a194dea8f0facedcc17b
juantoniogomezz <j.antonio1893@gmail.com>
2025-01-30 18:53:52 +0100

    operario funcionando en local

--- FRONT/src/main.ts
+++ FRONT/src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http'; // <--- Importa provideHttpClient
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [...(appConfig.providers || []), provideHttpClient()] // <--- Agrega HttpClient
+})
+.catch((err) => console.error(err));
